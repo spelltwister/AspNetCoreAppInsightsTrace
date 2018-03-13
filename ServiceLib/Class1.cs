2a3fff4263b649605bb8e2e8e3f683f94b937079
Michael Pavlak <michael.pavlak@luxinteractive.com>
2018-03-13 13:35:06 -0400

    Add project files.

---
+++ ServiceLib/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace ServiceLib
+{
+    public class ServiceClass
+    {
+        private static HttpClient Client { get; } = new HttpClient();
+
+        public async Task RunAvailabilityTestsAsync()
+        {
+            List<Exception> exs = new List<Exception>(2);
+
+            System.Diagnostics.Trace.TraceInformation("Beginning is google available method.");
+            try
+            {
+                await TryGetResponseWithRetryAsync("https://google.com").ConfigureAwait(false);
+                System.Diagnostics.Trace.TraceInformation("Google was successful.");
+            }
+            catch(Exception ex)
+            {
+                System.Diagnostics.Trace.TraceError($"Google failed with message: {ex.Message}");
+                exs.Add(ex);
+            }
+
+            try
+            {
+                await TryGetResponseWithRetryAsync("https://thisisagarbageendpoint.com").ConfigureAwait(false);
+                System.Diagnostics.Trace.TraceInformation("Garbage endpoint was successful somehow.");
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.TraceError($"Garbage endpoint failed with message: {ex.Message}");
+                exs.Add(ex);
+            }
+
+            if (exs.Any())
+            {
+                throw new AggregateException(exs);
+            }
+        }
+
+        private static async Task<HttpResponseMessage> TryGetResponseWithRetryAsync(string url)
+        {
+            int attempt = 0;
+            while (attempt++ < 4)
+            {
+                var response = await Client.GetAsync(url).ConfigureAwait(false);
+
+                System.Diagnostics.Trace.TraceInformation($"Send request to {url} and received a response.");
+
+                if (!response.IsSuccessStatusCode)
+                {
+                    System.Diagnostics.Trace.TraceWarning($"{url} response was not successful.");
+                }
+
+                return response;
+            }
+
+            System.Diagnostics.Trace.TraceError($"{url} failed after retries.");
+            throw new Exception($"Unable to get successful response after {attempt} attempts.");
+        }
+    }
+}
---
+++ TraceWithAppInsights/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ServiceLib;
+
+namespace TraceWithAppInsights.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        private static ServiceClass Service { get; } = new ServiceClass();
+
+        // GET api/values
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            try
+            {
+                await Service.RunAvailabilityTestsAsync().ConfigureAwait(false);
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(500);
+            }
+        }
+    }
+}
